e6e22950435f69e3315b2d79f1fdec623131d418
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-29 17:58:35 -0300

    Add two new form components

---
+++ components/form/Input.tsx
@@ -0,0 +1,37 @@
+import { twMerge } from 'tailwind-merge'
+import { forwardRef, InputHTMLAttributes } from 'react'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  isInvalid?: boolean
+  disabled?: boolean
+}
+
+const baseClasses =
+  'block w-full px-3 py-1 border border-slate-400 rounded-md focus:outline-none focus:ring-purple-500 focus:ring-1 focus:border-purple-500'
+
+const disabledClasses = 'bg-gray-100 text-gray-700 cursor-not-allowed'
+
+const invalidClasses = 'border-red-400 text-red-500 focus:border-red-400 focus:ring-red-400'
+
+const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  const { type = 'text', disabled = false, isInvalid = false, className, ...other } = props
+
+  return (
+    <input
+      type={type}
+      className={twMerge(
+        baseClasses,
+        disabled && disabledClasses,
+        isInvalid && invalidClasses,
+        className
+      )}
+      ref={ref}
+      disabled={disabled}
+      {...other}
+    />
+  )
+})
+
+Input.displayName = 'Input'
+
+export default Input
