3776c329a35e9b8f760befe8ad2f11c635615de9
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-29 18:00:24 -0300

    Add two new UI components

---
+++ components/ui/Alert.tsx
@@ -0,0 +1,21 @@
+import { twMerge } from 'tailwind-merge'
+import { ReactNode } from 'react'
+
+interface AlertProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: ReactNode
+  variant?: keyof typeof variants
+}
+
+const variants = {
+  primary: 'bg-blue-100 text-blue-700 border border-blue-700',
+  success: 'bg-emerald-100 text-emerald-700 border border-emerald-700',
+  danger: 'bg-red-100 text-red-700 border border-red-700'
+}
+
+const Alert = ({ children, variant = 'primary', className }: AlertProps) => {
+  return <div className={twMerge('rounded-md p-3', variants[variant], className)}>
+    {children}
+  </div>
+}
+
+export default Alert
---
+++ components/ui/MyButton.tsx
@@ -0,0 +1,36 @@
+import { twMerge } from 'tailwind-merge'
+import { forwardRef, ButtonHTMLAttributes } from 'react'
+
+const baseClasses =
+  'px-4 py-2 disabled:cursor-not-allowed transition-all focus:outline-none focus:ring-4 font-medium rounded-md'
+
+const classes = {
+  primary:
+    'text-white bg-purple-700 hover:bg-purple-800 focus:ring-purple-300 disabled:bg-purple-500',
+  black: 'text-white bg-black hover:bg-gray-800 focus:ring-black/30 disabled:bg-gray-600',
+  'outline-primary':
+    'text-purple-800 bg-white border border-purple-800 hover:bg-purple-800 hover:text-white focus:ring-purple-300 disabled:bg-black/10 disabled:text-purple-600 disabled:border-purple-600',
+  'transparent-primary':
+    'p-1 text-purple-800 hover:bg-purple-500/10 focus:ring-purple-200 disabled:bg-black/10',
+  'transparent-danger': 'p-1 text-red-500 hover:bg-red-100 focus:ring-red-300 disabled:bg-black/10'
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: keyof typeof classes
+}
+
+const MyButton = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, variant = 'primary', className, ...other }, ref) => (
+    <button
+      className={twMerge(baseClasses, classes[variant], className)}
+      ref={ref}
+      {...other}
+    >
+      {children}
+    </button>
+  )
+)
+
+MyButton.displayName = 'Button'
+
+export default MyButton
